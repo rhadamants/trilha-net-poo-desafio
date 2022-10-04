ce7d44f5fbdad0d4a92567341825796d51233c06
WandersonAReis <wandersonfrost8@hotmail.com>
2022-10-04 00:48:19 -0300

    Conclusão Desafio -> DIO - Programação orientada a objetos

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,29 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models;
+
+public abstract class Smartphone
 {
-    public abstract class Smartphone
+    public string Numero { get; set; }
+    private  string Modelo { get; }
+    private string IMEI { get; }
+    private int Memoria { get; }
+
+    protected Smartphone(string numero, string modelo, string imei, int memoria)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+        Numero = numero;
+        Modelo = modelo;
+        IMEI = imei;
+        Memoria = memoria;
     }
+
+    public void Ligar()
+    {
+        Console.WriteLine($"Ligando para {Numero}...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("31988888888", "Nokia", "11111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+nokia.ReceberLigacao();
+nokia.Numero = "31555555555";
+
+Smartphone iphone = new Iphone("31777777777", "Iphone", "33333333333", 120);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+iphone.Numero = "31444444444";
+iphone.Ligar();
